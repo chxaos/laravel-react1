a9dfc86df83dbc0a4ede0fd0611d43965384e288
Charl Lance Cua <clancecua@gmail.com>
2023-08-31 18:46:33 +0800

    Created temp survey items

--- react1/src/views/Surveys.jsx
+++ react1/src/views/Surveys.jsx
@@ -1,6 +1,10 @@
 import PageComponent from '../components/PageComponent';
+import { useStateContext } from '../context/ContextProvider';
 
 export default function Surveys() {
+  const { surveys } = useStateContext();
+  console.log(surveys);
+
   return (
     <>
       <PageComponent title="Surveys">Surveys Content</PageComponent>

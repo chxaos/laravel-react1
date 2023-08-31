3a930c6ff90fdc422e0c49b7745b6918568ad92e
chxaos <clancecua@gmail.com>
2023-09-01 02:14:46 +0800

    added fx

--- react1/src/components/SurveyListItem.jsx
+++ react1/src/components/SurveyListItem.jsx
@@ -8,7 +8,7 @@ import TButton from './core/TButton';
 
 export default function SurveyListItem({ survey, onDeleteClick }) {
   return (
-    <div className="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray h-[470px]">
+    <div className="flex flex-col py-4 px-6 shadow-md bg-white hover:shadow-2xl hover:shadow-teal-900 hover:ring-1 hover:ring-indigo-950/50 hover:scale-105  transition-transform h-[470px]">
       <img
         src={survey.image_url}
         alt={survey.title}
@@ -17,10 +17,10 @@ export default function SurveyListItem({ survey, onDeleteClick }) {
       <h4 className="mt-4 text-lg font-bold">{survey.title}</h4>
       <div
         dangerouslySetInnerHTML={{ __html: survey.description }}
-        className="overflow-hidden flex-1"
+        className="overflow-y-auto flex-1"
       ></div>
 
-      <div className="flex justify-between items-center mt-3">
+      <div className="flex justify-between items-center mt-5">
         <TButton to={`survey/${survey.id}`}>
           <PencilIcon className="w-5 h-5 mr-2" />
           Edit
